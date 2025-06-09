a9b23ce95c5b8629ec95b458a374996139c8e7a8
sihengt <siheng.teng@gmail.com>
2025-06-09 09:31:46 -0400

    Model updates

--- models/ResDSKBM.py
+++ models/ResDSKBM.py
@@ -1,7 +1,33 @@
 import numpy as np
 import casadi as cs
+import torch
+import l4casadi as l4c
+import os
+from learning.architecture.dynamicsModel import AdaptiveDynamicsModel
 
-class csDSKBM:
+torch.backends.mkldnn.enabled = False
+
+import torch.nn as nn
+class AirModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=3, hidden_size=50, num_layers=2, batch_first=True)
+        self.linear = nn.Linear(50, 1)
+
+    def forward(self, x):
+        # Reshape input from (15, 1) to (1, 5, 3)
+        #x = torch.tensor(x).reshape(1, 5, 3)
+        x = x.reshape(1, 5, 3)
+        print(x.shape)
+
+
+        # Passer à travers le LSTM
+        x, _ = self.lstm(x)
+        x = self.linear(x[:, -1, :])
+        return x
+
+
+class ResDSKBM:
     """
     Double Steer Kinematic Bicycle Model assumes:
         1. Velocity of each wheel points at the direction the wheel is facing (i.e. steering angle)
@@ -9,20 +35,15 @@ class csDSKBM:
         3. Tracks position and velocity of CG.
     """
 
-    def __init__(self, mpc_params):
+    def __init__(self, params):
         """
         Constructor
 
         Params:
-            n_states: 
-            n_actions: 
-            L: 
-            l_f: 
-            l_r: 
-            T: 
-            N: 
-            M: 
+            params: [dict] contains all the parameters required for MPC to run.
         """        
+        mpc_params = params['mpc']
+
         self.nX = 4
         self.mU = 3
         model_params = mpc_params['model']
@@ -63,9 +84,32 @@ class csDSKBM:
 
         self.get_A = self.init_A()
         self.get_B = self.init_B()
+        
         self.RK4 = self.init_rk4()
         
         self.dae = self.initialize_dae()
+
+        ## NEW CODE
+        self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        self.l4c_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.lstm = AdaptiveDynamicsModel(params['network'], params['controls'], mpc_params).to(self.device)
+        self.lstm.load_state_dict(torch.load(os.path.join(params['dataDir'],'adm.pt'), map_location=torch.device(self.device)))
+        self.l4c_model = l4c.L4CasADi(self.lstm)
+
+        # LSTM functions
+        self.lstm_input = cs.MX.sym('lstm_in', 1, (self.nX + self.mU) * params['train']['trainPredSeqLen'])        
+        self.lstm_out = self.l4c_model(self.lstm_input)
+
+        self.lstm_input_last_state = self.lstm_input[0, self.nX * params['train']['trainPredSeqLen'] - 1 : self.nX * params['train']['trainPredSeqLen'] - 1 + self.nX]
+        action_index_start = self.nX * params['train']['trainPredSeqLen']
+        self.lstm_input_last_action = self.lstm_input[0, action_index_start + self.mU * params['train']['trainPredSeqLen'] - 1 : action_index_start + self.mU * params['train']['trainPredSeqLen'] - 1 + self.mU]
+
+        self.get_lstm_A = self.init_res_A()
+        self.get_lstm_B = self.init_res_B()
+
+        
+        ##
+
 
     def init_sideslip_function(self):
         return cs.Function(
@@ -91,23 +135,13 @@ class csDSKBM:
     def init_B(self):
         return cs.Function('get_B', [self.X, self.U], [cs.jacobian(self.X_dot, self.U)])
 
-    def get_discrete_matrices(self, X_bar, U_bar):
-        A = self.get_A(X_bar, U_bar)
-        B = self.get_B(X_bar, U_bar)
-        
-        f_bar = self.f_x_dot(X=X_bar, U=U_bar)['X_dot']
-        g = f_bar - A @ X_bar - B @ U_bar
+    def init_res_A(self):
+        return cs.Function('get_A_res', [self.lstm_input], [cs.jacobian(self.lstm_out, self.lstm_input_last_state)])
 
-        A_exp_2 = A @ A
-        A_exp_3 = A_exp_2 @ A
-        A_exp_4 = A_exp_3 @ A
+    def init_res_B(self):
+        return cs.Function('get_B_res', [self.lstm_input], [cs.jacobian(self.lstm_out, self.lstm_input_last_action)])
 
-        A_d = cs.DM(np.eye(4)) + (self.dt * A) + (self.dt**2 / 2 * A_exp_2) + (self.dt**3/6 * A_exp_3) + (self.dt**4/24 * A_exp_4)
-        B_d = self.dt * B + (self.dt**2/2 * A @ B) + (self.dt**3/6 * A_exp_2 @ B) + (self.dt**4/24 * A_exp_3 @ B)
-        C_d = self.dt * g + self.dt**2/2 * A @ g + self.dt**3/6 * A_exp_2 @ g + self.dt**4/24 * A_exp_3 @ g     
-
-        return A_d, B_d, C_d
-
+    # TODO: include LSTM stuff
     def integrate(self, X_bar, U_bar):
         A = self.get_A(X_bar, U_bar)
         B = self.get_B(X_bar, U_bar)
